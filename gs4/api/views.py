3c9aa1be4bd76766b71b4618d2ea7bdd7c31faec
ankit-temp <iamjangidankit.com>
2025-02-28 12:43:57 +0530

    CRUD: class based views

---
+++ gs4/api/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from .serializers import StudentSerializer
+from .models import Student
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views import View
+
+#class based views.
+@method_decorator(csrf_exempt, name='dispatch')
+class StudentAPI(View): 
+    def get(self, request, *args, **kwargs):
+        json_data = request.body
+        print('GET')
+        print(json_data)
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id', None)
+
+        #deserialize and send data back to client
+        if id is not None:
+            student = Student.objects.get(id=id)
+            serializer = StudentSerializer(student)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+        print('GOTTA GIVE EM ALL')
+        #if no id then send all data
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
+
+    #create data
+    def post(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=python_data)
+        if serializer.is_valid():
+            serializer.save()
+            ack = {'msg': 'Data Created'}
+            json_data = JSONRenderer().render(ack)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    def put(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        #get data from db for the student with id
+        student = Student.objects.get(id=id)
+        #mark partial=True since this is a partial update
+        serializer = StudentSerializer(student, data=python_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            ack = {'msg': 'Data Updated'}
+            json_data = JSONRenderer().render(ack)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    def delete(self, request, *args, **kwargs):
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        student = Student.objects.get(id=id)
+        student.delete()
+        ack = {'msg': 'Data Deleted'}
+        json_data = JSONRenderer().render(ack)
+        return HttpResponse(json_data, content_type='application/json')
