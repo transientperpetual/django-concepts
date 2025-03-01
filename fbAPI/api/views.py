2b35dcd5941975b74d9281abdeaacb822ccd51ba
transientperpetual <iamjangidankit@gmail.com>
2025-03-01 18:55:56 +0530

    function based API_VIEWs

---
+++ fbAPI/api/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import status
+
+# Create your views here.
+# @api_view()
+# def hello_world(request):
+#     return Response({"message": "Hello, world!"})
+
+
+# @api_view(['POST'])
+# def hello_world(request):
+#     print(request.data)
+#     return Response({"message": "THis is post req!"})
+
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def studentAPI(request):
+    if request.method == 'GET':
+        id = request.data.get('id')
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        id = request.data.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Updated!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        id = request.data.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        return Response({'msg': 'Data Deleted!'})
