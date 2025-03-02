0abf7a24c9b7e6a60373ae524b5c7b04478926bb
transientperpetual <iamjangidankit@gmail.com>
2025-03-02 13:52:47 +0530

    all concrete views covered

--- concreteViewClass/api/views.py
+++ concreteViewClass/api/views.py
@@ -1,6 +1,6 @@
 from .serializers import StudentSerializer
 from .models import Student 
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView, ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
@@ -23,5 +23,24 @@ class StudentDestroy(DestroyAPIView):
     serializer_class = StudentSerializer
 
 
+#combined views:
+
+class StudentListCreate(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveDestroy(RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+*Note: StudentListCreate & StudentRetrieveUpdateDestroy are enough to perfrom CRUD operations.
  
 
